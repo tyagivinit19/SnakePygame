7b47c91ecc9de9adcf9809648faf441ff929b6c8
tyagi <tyagivinit19@gmail.com>
2020-02-03 19:06:16 +0530

    add loop

--- Snake.py
+++ Snake.py
@@ -3,8 +3,14 @@ import pygame
 pygame.init()
 
 gameDisplay = pygame.display.set_mode((800, 600))
-
+pygame.display.set_caption("Snake")
 pygame.display.update()
+
+gameExit = False
+
+while not gameExit:
+    for event in pygame.event.get():
+        print(event)
 
 pygame.quit()
 
